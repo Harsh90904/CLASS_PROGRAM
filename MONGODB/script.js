ecbb8306818632b0ef3b0f8ed9bd77547aadcdc4
Harsh_Diyora <146904128+Harsh90904@users.noreply.github.com>
2024-09-04 11:55:49 +0530

    Update script.js

--- MONGODB/script.js
+++ MONGODB/script.js
@@ -12,14 +12,8 @@ app.get("/read", async (req, res) => {
 });
 
 app.post("/add", async (req, res) => {
-    let {username , password , email} = req.params;
-    if(username == username || password == password || email == email){
-        res.send("user already exists");
-    }
-    else{
-        let data = await User.create(req.body );
-        res.send(data);
-    }
+    let data = await User.create(req.body);
+    res.send(data);
 })
 app.patch("/update/:id", async (req, res)=> {
     let { id } = req.params;
